fd28158df83691fbfbadeebcb194fe9071ef6e92
Evgeniy K <pgg1337@mail.ru>
2023-10-18 16:23:24 +0300

    commit 3

---
+++ service/services/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from services.models import Subscription
+
+
+class SubscriptionSerializers(serializers.ModelSerializer):
+    client_name = serializers.CharField(source='client.company_name')
+    email = serializers.CharField(source='client.user.email')
+
+    class Meta:
+        model = Subscription
+        fields = ('id', 'plan_id', 'client_name', 'email')
+
+
--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from clients.models import Client
+from services.models import Subscription
+from .serializers import SubscriptionSerializers
+
+
+class SubscriptionView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = SubscriptionSerializers
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related(
+            'user').only('company_name',
+                         'user__email'))
+    )
